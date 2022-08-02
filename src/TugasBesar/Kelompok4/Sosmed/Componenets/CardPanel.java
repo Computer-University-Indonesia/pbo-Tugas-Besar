df470a7462b8e19741f61ab092e82c5b83f69426
Farizside <muhammadfarizh31@gmail.com>
2022-08-02 10:35:58 +0700

    [ADD] Loba weh pokonamah poho deui aing

--- src/TugasBesar/Kelompok4/Sosmed/Componenets/CardPanel.java
+++ src/TugasBesar/Kelompok4/Sosmed/Componenets/CardPanel.java
@@ -5,7 +5,9 @@
  */
 package TugasBesar.Kelompok4.Sosmed.Componenets;
 
+import java.awt.Color;
 import javax.swing.JLabel;
+import javax.swing.border.LineBorder;
 
 /**
  *
@@ -13,6 +15,7 @@ import javax.swing.JLabel;
  */
 public class CardPanel extends javax.swing.JPanel {
 
+    public boolean isLiked = false;
     /**
      * Creates new form CardPanel
      */
@@ -46,59 +49,157 @@ public class CardPanel extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        backgorundPanel = new javax.swing.JPanel();
         TXTTittle = new javax.swing.JLabel();
-        TXTDesc = new javax.swing.JLabel();
         TXTDate = new javax.swing.JLabel();
         TXTName = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        TXTDesc = new javax.swing.JTextArea();
+        likeButton = new javax.swing.JToggleButton();
+        TXTLike = new javax.swing.JLabel();
+        detailButton = new javax.swing.JPanel();
+        detailText = new javax.swing.JLabel();
 
-        setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        setBackground(new java.awt.Color(255, 255, 255));
+        setPreferredSize(new java.awt.Dimension(1440, 284));
+        setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        backgorundPanel.setBackground(new java.awt.Color(255, 255, 255));
+        backgorundPanel.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(240, 240, 240), 125, true));
+        backgorundPanel.setPreferredSize(new java.awt.Dimension(1380, 250));
+        backgorundPanel.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                backgorundPanelMouseClicked(evt);
+            }
+        });
+        backgorundPanel.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         TXTTittle.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
-        TXTTittle.setText("Tittle");
-
-        TXTDesc.setText("Desc");
-        TXTDesc.setVerticalAlignment(javax.swing.SwingConstants.TOP);
+        TXTTittle.setText("Title");
+        TXTTittle.setPreferredSize(new java.awt.Dimension(400, 30));
+        backgorundPanel.add(TXTTittle, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 60, -1, -1));
 
         TXTDate.setText("Date");
+        backgorundPanel.add(TXTDate, new org.netbeans.lib.awtextra.AbsoluteConstraints(1230, 210, -1, -1));
 
+        TXTName.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
         TXTName.setText("Nama");
+        TXTName.setPreferredSize(new java.awt.Dimension(400, 30));
+        backgorundPanel.add(TXTName, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 20, -1, -1));
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
-        this.setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(TXTDesc, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(TXTTittle, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(TXTDate)
-                            .addComponent(TXTName, javax.swing.GroupLayout.PREFERRED_SIZE, 186, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGap(0, 1224, Short.MAX_VALUE)))
-                .addContainerGap())
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(TXTTittle)
-                .addGap(1, 1, 1)
-                .addComponent(TXTName)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(TXTDesc, javax.swing.GroupLayout.PREFERRED_SIZE, 56, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 121, Short.MAX_VALUE)
-                .addComponent(TXTDate)
-                .addContainerGap())
-        );
+        jScrollPane1.setHorizontalScrollBarPolicy(javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        jScrollPane1.setPreferredSize(new java.awt.Dimension(1300, 90));
+
+        TXTDesc.setEditable(false);
+        TXTDesc.setColumns(20);
+        TXTDesc.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        TXTDesc.setLineWrap(true);
+        TXTDesc.setRows(3);
+        jScrollPane1.setViewportView(TXTDesc);
+
+        backgorundPanel.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 100, -1, -1));
+
+        likeButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/TugasBesar/Kelompok4/Sosmed/Componenets/heart.png"))); // NOI18N
+        likeButton.setPreferredSize(new java.awt.Dimension(50, 30));
+        likeButton.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                likeButtonMouseClicked(evt);
+            }
+        });
+        backgorundPanel.add(likeButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 205, -1, -1));
+
+        TXTLike.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        TXTLike.setText("100");
+        TXTLike.setPreferredSize(new java.awt.Dimension(40, 30));
+        backgorundPanel.add(TXTLike, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 205, -1, -1));
+
+        detailButton.setBackground(new java.awt.Color(241, 241, 241));
+        detailButton.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(21, 115, 255), 25, true));
+        detailButton.setPreferredSize(new java.awt.Dimension(100, 30));
+        detailButton.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                detailButtonMouseClicked(evt);
+            }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                detailButtonMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                detailButtonMouseExited(evt);
+            }
+        });
+        detailButton.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        detailText.setBackground(new java.awt.Color(241, 241, 241));
+        detailText.setForeground(new java.awt.Color(255, 255, 255));
+        detailText.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        detailText.setText("Detail");
+        detailText.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        detailText.setPreferredSize(new java.awt.Dimension(40, 30));
+        detailText.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                detailTextMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                detailTextMouseExited(evt);
+            }
+        });
+        detailButton.add(detailText, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 100, -1));
+
+        backgorundPanel.add(detailButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 205, -1, -1));
+
+        add(backgorundPanel, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 20, -1, -1));
     }// </editor-fold>//GEN-END:initComponents
+
+    private void likeButtonMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_likeButtonMouseClicked
+        // TODO add your handling code here:
+        isLiked = !isLiked;
+        if(isLiked == true){
+            this.TXTLike.setText("200");
+        }else{
+            this.TXTLike.setText("500");
+        }
+    }//GEN-LAST:event_likeButtonMouseClicked
+
+    private void backgorundPanelMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_backgorundPanelMouseClicked
+        // TODO add your handling code here:
+    }//GEN-LAST:event_backgorundPanelMouseClicked
+
+    private void detailButtonMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_detailButtonMouseEntered
+        // TODO add your handling code here:
+        detailButton.setBorder(new LineBorder(new Color(1, 95, 235), 25, true));
+    }//GEN-LAST:event_detailButtonMouseEntered
+
+    private void detailButtonMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_detailButtonMouseExited
+        // TODO add your handling code here:
+        detailButton.setBorder(new LineBorder(new Color(21, 115, 255), 25, true));
+    }//GEN-LAST:event_detailButtonMouseExited
+
+    private void detailButtonMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_detailButtonMouseClicked
+        // TODO add your handling code here:
+        System.out.println("Detail Post");
+    }//GEN-LAST:event_detailButtonMouseClicked
+
+    private void detailTextMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_detailTextMouseEntered
+        // TODO add your handling code here:
+        detailButton.setBorder(new LineBorder(new Color(1, 95, 235), 25, true));
+    }//GEN-LAST:event_detailTextMouseEntered
+
+    private void detailTextMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_detailTextMouseExited
+        // TODO add your handling code here:
+        detailButton.setBorder(new LineBorder(new Color(21, 115, 255), 25, true));
+    }//GEN-LAST:event_detailTextMouseExited
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel TXTDate;
-    private javax.swing.JLabel TXTDesc;
+    private javax.swing.JTextArea TXTDesc;
+    private javax.swing.JLabel TXTLike;
     private javax.swing.JLabel TXTName;
     private javax.swing.JLabel TXTTittle;
+    private javax.swing.JPanel backgorundPanel;
+    private javax.swing.JPanel detailButton;
+    private javax.swing.JLabel detailText;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JToggleButton likeButton;
     // End of variables declaration//GEN-END:variables
 }
--- src/TugasBesar/Kelompok4/Sosmed/Views/HomeView.java
+++ src/TugasBesar/Kelompok4/Sosmed/Views/HomeView.java
@@ -5,16 +5,13 @@
  */
 package TugasBesar.Kelompok4.Sosmed.Views;
 
-import TugasBesar.Kelompok4.Sosmed.Componenets.CardDashboardLabel;
-import TugasBesar.Kelompok4.Sosmed.Componenets.CardPanel;
-import TugasBesar.Kelompok4.Sosmed.Componenets.CreatePostPanel;
+import TugasBesar.Kelompok4.Sosmed.Componenets.*;
+import TugasBesar.Kelompok4.Sosmed.Constants.Authentication;
 import TugasBesar.Kelompok4.Sosmed.Controllers.PostController;
 import java.awt.Color;
 import java.util.Stack;
 import javax.swing.BoxLayout;
-import javax.swing.JFrame;
-import javax.swing.JScrollBar;
-import javax.swing.JScrollPane;
+import javax.swing.border.LineBorder;
 
 /**
  *
@@ -26,15 +23,16 @@ public class HomeView extends javax.swing.JFrame {
      * Creates new form HomeView
      */
     PostController controller;
+    
 
     public HomeView() {
         initComponents();
         this.setResizable(false);
-        this.setExtendedState(JFrame.MAXIMIZED_HORIZ);
         controller = new PostController();
         homeButton.setText("Home");
         profileButton.setText("Profile");
         homeButton.isAtctive(true);
+        nama.setText(Authentication.getName());
         loadPost();
     }
 
@@ -55,9 +53,19 @@ public class HomeView extends javax.swing.JFrame {
         jPanel1 = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
         jPanel6 = new javax.swing.JPanel();
-        createPostPanel1 = new TugasBesar.Kelompok4.Sosmed.Componenets.CreatePostPanel();
+        backgroundPanel = new javax.swing.JPanel();
+        namaPanel = new javax.swing.JPanel();
+        nama = new javax.swing.JLabel();
+        backgroundTitle = new javax.swing.JPanel();
+        titleField = new javax.swing.JTextField();
+        backgroundPost = new javax.swing.JPanel();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        postField = new javax.swing.JTextArea();
+        postButton = new javax.swing.JPanel();
+        jLabel2 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setPreferredSize(new java.awt.Dimension(1440, 1024));
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jPanel2.setBackground(new java.awt.Color(255, 255, 255));
@@ -98,33 +106,116 @@ public class HomeView extends javax.swing.JFrame {
         jScrollPane1.setBorder(null);
         jScrollPane1.setForeground(new java.awt.Color(153, 153, 255));
         jScrollPane1.setHorizontalScrollBarPolicy(javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-        jScrollPane1.setVerticalScrollBarPolicy(javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
         jScrollPane1.setPreferredSize(new java.awt.Dimension(1440, 600));
 
         jPanel6.setBackground(new java.awt.Color(255, 255, 255));
         jPanel6.setLayout(new javax.swing.BoxLayout(jPanel6, javax.swing.BoxLayout.LINE_AXIS));
         jScrollPane1.setViewportView(jPanel6);
 
-        jPanel1.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 290, 1480, -1));
+        jPanel1.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 290, -1, -1));
 
-        createPostPanel1.addMouseListener(new java.awt.event.MouseAdapter() {
+        backgroundPanel.setBackground(new java.awt.Color(255, 255, 255));
+        backgroundPanel.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(240, 240, 240), 125, true));
+        backgroundPanel.setPreferredSize(new java.awt.Dimension(1400, 250));
+        backgroundPanel.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        namaPanel.setBackground(new java.awt.Color(241, 241, 241));
+        namaPanel.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(255, 255, 255), 50, true));
+        namaPanel.setPreferredSize(new java.awt.Dimension(300, 50));
+        namaPanel.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        nama.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
+        nama.setText("Nama");
+        nama.setPreferredSize(new java.awt.Dimension(260, 50));
+        namaPanel.add(nama, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 0, -1, -1));
+
+        backgroundPanel.add(namaPanel, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 20, -1, -1));
+
+        backgroundTitle.setBackground(new java.awt.Color(241, 241, 241));
+        backgroundTitle.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(255, 255, 255), 50, true));
+        backgroundTitle.setPreferredSize(new java.awt.Dimension(300, 50));
+        backgroundTitle.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        titleField.setText("Title");
+        titleField.setBorder(null);
+        titleField.setPreferredSize(new java.awt.Dimension(260, 50));
+        titleField.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
-                createPostPanel1MouseClicked(evt);
+                titleFieldMouseClicked(evt);
             }
         });
-        jPanel1.add(createPostPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
+        backgroundTitle.add(titleField, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 0, -1, -1));
+
+        backgroundPanel.add(backgroundTitle, new org.netbeans.lib.awtextra.AbsoluteConstraints(340, 20, -1, -1));
+
+        backgroundPost.setBackground(new java.awt.Color(241, 241, 241));
+        backgroundPost.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(255, 255, 255), 50, true));
+        backgroundPost.setPreferredSize(new java.awt.Dimension(1360, 100));
+        backgroundPost.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jScrollPane2.setBorder(null);
+        jScrollPane2.setHorizontalScrollBarPolicy(javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        jScrollPane2.setVerticalScrollBarPolicy(javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER);
+
+        postField.setColumns(20);
+        postField.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        postField.setRows(5);
+        jScrollPane2.setViewportView(postField);
+
+        backgroundPost.add(jScrollPane2, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 0, 1320, 100));
+
+        backgroundPanel.add(backgroundPost, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 90, -1, -1));
+
+        postButton.setBackground(new java.awt.Color(241, 241, 241));
+        postButton.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(21, 115, 255), 25, true));
+        postButton.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        postButton.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                postButtonMouseClicked(evt);
+            }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                postButtonMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                postButtonMouseExited(evt);
+            }
+        });
+        postButton.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel2.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel2.setText("Post");
+        jLabel2.setPreferredSize(new java.awt.Dimension(150, 40));
+        jLabel2.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jLabel2MouseClicked(evt);
+            }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                jLabel2MouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                jLabel2MouseExited(evt);
+            }
+        });
+        postButton.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
+
+        backgroundPanel.add(postButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(1170, 200, -1, -1));
+
+        jPanel1.add(backgroundPanel, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 17, -1, -1));
 
         jPanel2.add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 100, -1, -1));
 
         getContentPane().add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void profileButtonMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_profileButtonMouseClicked
         // TODO add your handling code here:
-        profileButton.isAtctive(true);
-        homeButton.isAtctive(false);
+        this.setVisible(false);
+        new ProfileView().setVisible(true);
 
     }//GEN-LAST:event_profileButtonMouseClicked
 
@@ -135,13 +226,46 @@ public class HomeView extends javax.swing.JFrame {
 
     }//GEN-LAST:event_homeButtonMouseClicked
 
-    private void createPostPanel1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_createPostPanel1MouseClicked
+    private void jLabel2MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel2MouseClicked
         // TODO add your handling code here:
-        boolean created = CreatePostPanel.isCreated;    
-        if(created){
-            loadPost();
-        }
-    }//GEN-LAST:event_createPostPanel1MouseClicked
+        String[] inputs = {titleField.getText(), postField.getText()};
+
+        controller.store(inputs);
+        loadPost();
+        this.titleField.setText("Title");
+        this.postField.setText("");
+    }//GEN-LAST:event_jLabel2MouseClicked
+
+    private void jLabel2MouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel2MouseEntered
+        // TODO add your handling code here:
+
+        postButton.setBorder(new LineBorder(new Color(1, 95, 235), 25, true));
+    }//GEN-LAST:event_jLabel2MouseEntered
+
+    private void jLabel2MouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel2MouseExited
+        // TODO add your handling code here:
+        postButton.setBorder(new LineBorder(new Color(21, 115, 255), 25, true));
+    }//GEN-LAST:event_jLabel2MouseExited
+
+    private void postButtonMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_postButtonMouseClicked
+        // TODO add your handling code here:
+    }//GEN-LAST:event_postButtonMouseClicked
+
+    private void postButtonMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_postButtonMouseEntered
+        // TODO add your handling code here:
+        postButton.setBorder(new LineBorder(new Color(1, 95, 235), 25, true));
+        System.out.println("mouse enter");
+    }//GEN-LAST:event_postButtonMouseEntered
+
+    private void postButtonMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_postButtonMouseExited
+        // TODO add your handling code here:
+        postButton.setBorder(new LineBorder(new Color(21, 115, 255), 25, true));
+    }//GEN-LAST:event_postButtonMouseExited
+
+    private void titleFieldMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_titleFieldMouseClicked
+        // TODO add your handling code here:
+        this.titleField.setText("");
+    }//GEN-LAST:event_titleFieldMouseClicked
 
     /**
      * @param args the command line arguments
@@ -154,7 +278,6 @@ public class HomeView extends javax.swing.JFrame {
         jPanel6.revalidate();
         for (Stack<String> row : data) {
             CardPanel card = new CardPanel();
-            System.out.println(row);
 
             card.setName(row.get(0));
             card.setTitle(row.get(1));
@@ -199,14 +322,23 @@ public class HomeView extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private TugasBesar.Kelompok4.Sosmed.Componenets.CreatePostPanel createPostPanel1;
+    private javax.swing.JPanel backgroundPanel;
+    private javax.swing.JPanel backgroundPost;
+    private javax.swing.JPanel backgroundTitle;
     private TugasBesar.Kelompok4.Sosmed.Componenets.CardDashboardLabel homeButton;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
     private javax.swing.JPanel jPanel6;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JLabel nama;
+    private javax.swing.JPanel namaPanel;
+    private javax.swing.JPanel postButton;
+    private javax.swing.JTextArea postField;
     private TugasBesar.Kelompok4.Sosmed.Componenets.CardDashboardLabel profileButton;
+    private javax.swing.JTextField titleField;
     // End of variables declaration//GEN-END:variables
 }
